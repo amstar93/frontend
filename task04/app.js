fe3fda2bdadbe3f051ddb472fed8571c0c1fbeed
amstar93 <102961387+amstar93@users.noreply.github.com>
2022-04-18 22:19:44 +0300

    123

--- task04/app.js
+++ task04/app.js
@@ -2,26 +2,42 @@ let word = "сосна";
 let input = document.querySelector("input")
 let buttonSubmit = document.querySelector("button")
 function ClickButtonSubmit () {
-    if (word === input.value){
+    let resultColors = checkWords(word, input.value)
+    console.log(resultColors)
+    let resultsElement = document.querySelector(".results")
+    for (let i = 0; i < 5; i++){
+        resultsElement.innerHTML += "<button class=btn style='background-color: " + resultColors[i] + "'>" + resultColors[i] + "</button>"
+    }
+    resultsElement.innerHTML += "<br>"  
+} 
+
+buttonSubmit.onclick = ClickButtonSubmit
+
+function checkWords(initialWord, userWord){
+    let colors = []
+    if (initialWord === userWord){
         console.log("ok")
     } else {
         console.log("no")
     }
-    console.log(input.value)
+    console.log(userWord)
     for (let i = 0; i < 5; i++){
-        console.log(word[i])
-        console.log(input.value[i])
-        if (word[i] === input.value[i]){
+        console.log(initialWord[i])
+        console.log(userWord[i])
+        if (initialWord[i] === userWord[i]){
             console.log("green")
-        } else  if (CheckLetInWord(input.value[i], word)){
+            colors.push("green")
+        } else  if (CheckLetInWord(userWord[i], initialWord)){
             console.log("yellow")
+            colors.push("yellow")
         } else{
             console.log("grey")
+            colors.push("grey")
         }
-
     }
-} 
-buttonSubmit.onclick = ClickButtonSubmit
+    return colors;
+}
+
 function CheckLetInWord(letter, word){
     // let letter = "п"
     // let word = "пират"
@@ -37,3 +53,6 @@ function CheckLetInWord(letter, word){
 
 let output = CheckLetInWord("g", "pirat")
 console.log(output)
+
+let res = checkWords("сосна", "сосан")
+console.log(res)
